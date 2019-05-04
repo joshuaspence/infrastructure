28255e4fa5eca58d2daf7c20b8f77c3f9502cb17
Joshua Spence <josh@joshuaspence.com>
2019-05-04 15:01:20 +1000

    Initial commit

---
+++ dns.tf
@@ -0,0 +1,85 @@
+/**
+ * TODO: Use `for_each` instead of `count`.
+ */
+
+variable "domains" {
+  type = map(object({
+    dkim                     = string
+    google_site_verification = string
+  }))
+}
+
+locals {
+  domain_count = length(var.domains)
+  domain_names = keys(var.domains)
+}
+
+resource "aws_route53_zone" "main" {
+  name = local.domain_names[count.index]
+
+  count = local.domain_count
+}
+
+resource "aws_route53_record" "dkim" {
+  zone_id = aws_route53_zone.main[count.index].zone_id
+  name    = "google._domainkey"
+  type    = "TXT"
+  ttl     = 60 * 60
+  records = ["v=DKIM1; k=rsa; p=${var.domains[local.domain_names[count.index]]["dkim"]}"]
+
+  count = local.domain_count
+}
+
+resource "aws_route53_record" "google_site_verification" {
+  zone_id = aws_route53_zone.main[count.index].zone_id
+  name    = ""
+  type    = "TXT"
+  ttl     = 60 * 60 * 24
+  records = ["google-site-verification=${var.domains[local.domain_names[count.index]]["google_site_verification"]}"]
+
+  count = local.domain_count
+}
+
+resource "aws_route53_record" "mx" {
+  zone_id = aws_route53_zone.main[count.index].zone_id
+  name    = ""
+  type    = "MX"
+  ttl     = 60 * 5
+
+  records = [
+    "1 aspmx.l.google.com.",
+    "5 alt1.aspmx.l.google.com.",
+    "5 alt2.aspmx.l.google.com.",
+    "10 aspmx2.googlemail.com.",
+    "10 aspmx3.googlemail.com.",
+  ]
+
+  count = local.domain_count
+}
+
+resource "aws_route53_record" "spf" {
+  zone_id = aws_route53_zone.main[count.index].zone_id
+  name    = ""
+  type    = "SPF"
+  ttl     = 60 * 5
+  records = ["v=spf1 a include:_spf.google.com ~all"]
+
+  count = local.domain_count
+}
+
+resource "aws_route53_record" "xmpp" {
+  zone_id = aws_route53_zone.main[count.index].zone_id
+  name    = "_xmpp-server._tcp"
+  type    = "SRV"
+  ttl     = 60 * 60
+
+  records = [
+    "20 0 5269 alt3.xmpp-server.l.google.com.",
+    "20 0 5269 alt2.xmpp-server.l.google.com.",
+    "20 0 5269 alt1.xmpp-server.l.google.com.",
+    "20 0 5269 alt4.xmpp-server.l.google.com.",
+    "5 0 5269 xmpp-server.l.google.com.",
+  ]
+
+  count = local.domain_count
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region              = "ap-southeast-2"
+  profile             = "personal"
+  allowed_account_ids = ["287139315271"]
+}
