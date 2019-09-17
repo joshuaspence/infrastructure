4f1d34f384f99e33dc3d308bcbb765ed285d96b2
Joshua Spence <josh@joshuaspence.com>
2019-09-18 08:16:18 +1000

    Don't manage default VPC and subnet

--- vpc.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_default_vpc" "main" {}
-
-resource "aws_default_subnet" "main" {
-  availability_zone = each.value
-  for_each          = toset(data.aws_availability_zones.available.names)
-}
