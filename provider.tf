7d0d7eee3711c7701f01b3b5c1f28c9e8cb1e7c3
Joshua Spence <josh@joshuaspence.com>
2019-10-04 08:32:48 +1000

    Remove the `helm` provider (temporarily)

--- provider.tf
+++ provider.tf
@@ -44,24 +44,6 @@ provider "gsuite" {
 }
 
 #===============================================================================
-# Helm
-#===============================================================================
-
-provider "helm" {
-  host            = module.eks.cluster_endpoint
-  namespace       = kubernetes_service_account.tiller.metadata[0].namespace
-  install_tiller  = true
-  service_account = kubernetes_service_account.tiller.metadata[0].name
-
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    token                  = data.aws_eks_cluster_auth.main.token
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    load_config_file       = false
-  }
-}
-
-#===============================================================================
 # Kubernetes
 #===============================================================================
 
