9bedbc6657bed023ed7b218d0a98b84df4e4e116
Joshua Spence <josh@joshuaspence.com>
2019-10-11 08:31:30 +1100

    Remove Tiller resources
    [Tiller is being removed from Helm 3](https://medium.com/better-programming/helm-3-fun-with-the-new-beta-8f91c70891ff), which is scheduled to be released soon.

--- kubernetes.tf
+++ kubernetes.tf
@@ -8,29 +8,3 @@ resource "kubernetes_config_map" "aws_auth" {
 
   data = yamldecode(module.eks.config_map_aws_auth)["data"]
 }
-
-# TODO: Check this.
-resource "kubernetes_service_account" "tiller" {
-  metadata {
-    name      = "tiller"
-    namespace = "kube-system"
-  }
-}
-
-resource "kubernetes_cluster_role_binding" "tiller" {
-  metadata {
-    name = "tiller"
-  }
-
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
-
-  subject {
-    kind      = "ServiceAccount"
-    name      = kubernetes_service_account.tiller.metadata[0].name
-    namespace = kubernetes_service_account.tiller.metadata[0].namespace
-  }
-}
