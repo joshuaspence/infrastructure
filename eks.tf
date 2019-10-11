a07591301546f554fa651771e9fbbffaa14bc94c
Joshua Spence <josh@joshuaspence.com>
2019-10-11 17:42:14 +1100

    Merge `eks.tf` and `kubernetes.tf`

--- eks.tf
+++ eks.tf
@@ -70,3 +70,14 @@ resource "local_file" "kubeconfig" {
   content  = module.eks.kubeconfig
   filename = pathexpand("~/.kube/config")
 }
+
+# TODO: This should be managed by the `terraform-aws-modules/eks/aws` modul
+# after https://github.com/terraform-aws-modules/terraform-aws-eks/pull/355
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = yamldecode(module.eks.config_map_aws_auth)["data"]
+}
