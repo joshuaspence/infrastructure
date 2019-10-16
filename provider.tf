718a39e99e89ceb3a4dc49a2ad392570b65673db
Joshua Spence <josh@joshuaspence.com>
2019-10-16 17:45:39 +1100

    Add the Helm provider

--- provider.tf
+++ provider.tf
@@ -53,6 +53,22 @@ provider "gsuite" {
 }
 
 #===============================================================================
+# Helm
+#===============================================================================
+
+provider "helm" {
+  namespace       = kubernetes_service_account.tiller.metadata[0].namespace
+  service_account = kubernetes_service_account.tiller.metadata[0].name
+
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    token                  = data.aws_eks_cluster_auth.main.token
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    load_config_file       = false
+  }
+}
+
+#===============================================================================
 # Kubernetes
 #===============================================================================
 
