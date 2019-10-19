6db939ce86ae3f15376a4d4a103db45e4b251fad
Joshua Spence <josh@joshuaspence.com>
2019-10-20 09:18:48 +1100

    Use `set` instead of `values` for `helm_release`

--- helm.tf
+++ helm.tf
@@ -1,22 +1,30 @@
-locals {
-  autoscaler_config = {
-    rbac = {
-      create = true
-    }
-
-    cloudProvider = "aws"
-    awsRegion     = var.aws_region
-
-    autoDiscovery = {
-      clusterName = module.eks.cluster_id
-      enabled     = true
-    }
-  }
-}
-
 # TODO: Check version of `cluster-autoscaler`.
 resource "helm_release" "autoscaler" {
   name   = "autoscaler"
   chart  = "stable/cluster-autoscaler"
-  values = [yamlencode(local.autoscaler_config)]
+
+  set {
+    name  = "autoDiscovery.clusterName"
+    value = module.eks.cluster_id
+  }
+
+  set {
+    name  = "autoDiscovery.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "awsRegion"
+    value = var.aws_region
+  }
+
+  set {
+    name  = "cloudProvider"
+    value = "aws"
+  }
+
+  set {
+    name  = "rbac.create"
+    value = "true"
+  }
 }
