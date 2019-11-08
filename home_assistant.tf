db5eaaf6df91de2075892cf62ad2f13a1af3b91d
Joshua Spence <josh@joshuaspence.com>
2019-11-09 09:10:00 +1100

    Split up `helm.tf` into separate files

---
+++ home_assistant.tf
@@ -0,0 +1,27 @@
+variable "home_assistant_config" {
+  type = object({
+    host    = string
+    version = string
+  })
+}
+
+# TODO: Should this be installed into its own namespace?
+resource "helm_release" "home_assistant" {
+  name  = "home-assistant"
+  chart = "stable/home-assistant"
+
+  set {
+    name  = "image.tag"
+    value = var.home_assistant_config.version
+  }
+
+  set {
+    name  = "ingress.enabled"
+    value = true
+  }
+
+  set {
+    name  = "ingress.hosts"
+    value = format("{%s}", var.home_assistant_config.host)
+  }
+}
--- prometheus.tf
+++ prometheus.tf
@@ -1,31 +1,3 @@
-variable "home_assistant_config" {
-  type = object({
-    host    = string
-    version = string
-  })
-}
-
-# TODO: Should this be installed into its own namespace?
-resource "helm_release" "home_assistant" {
-  name  = "home-assistant"
-  chart = "stable/home-assistant"
-
-  set {
-    name  = "image.tag"
-    value = var.home_assistant_config.version
-  }
-
-  set {
-    name  = "ingress.enabled"
-    value = true
-  }
-
-  set {
-    name  = "ingress.hosts"
-    value = format("{%s}", var.home_assistant_config.host)
-  }
-}
-
 variable "prometheus_config" {
   type = object({
     hosts = object({
