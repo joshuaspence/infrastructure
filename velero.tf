4617d80eaa49f1f1823a31a4a0d9238d8519eec9
Joshua Spence <josh@joshuaspence.com>
2019-11-16 18:15:54 +1100

    Add bucket policy for Velero

--- velero.tf
+++ velero.tf
@@ -4,6 +4,42 @@ resource "aws_s3_bucket" "velero" {
   bucket = "spence-velero"
 }
 
+resource "aws_s3_bucket_policy" "velero" {
+  bucket = aws_s3_bucket.velero.id
+  policy = data.aws_iam_policy_document.velero.json
+}
+
+# TODO: Can these permissions be further restricted?
+# TODO: Add conditions (e.g. `aws:SourceIp`).
+data "aws_iam_policy_document" "velero" {
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.velero.arn]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_user.velero.arn]
+    }
+  }
+
+  statement {
+    actions = [
+      "s3:AbortMultipartUpload",
+      "s3:DeleteObject",
+      "s3:GetObject",
+      "s3:ListMultipartUploadParts",
+      "s3:PutObject",
+    ]
+
+    resources = [format("%s/*", aws_s3_bucket.velero.arn)]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_user.velero.arn]
+    }
+  }
+}
+
 # TODO: Add a permissions boundary?
 resource "aws_iam_user" "velero" {
   name = "velero"
