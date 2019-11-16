4057095c5bb2c079cd7c5e10656eaa48f59a3fe9
Joshua Spence <josh@joshuaspence.com>
2019-11-16 18:30:15 +1100

    Add Velero

--- velero.tf
+++ velero.tf
@@ -45,3 +45,47 @@ resource "aws_iam_user" "velero" {
   name = "velero"
   path = "/system/"
 }
+
+resource "aws_iam_access_key" "velero" {
+  user = aws_iam_user.velero.name
+}
+
+# TODO: Update to the latest version (1.2.0).
+# TODO: Encrypt backups.
+# TODO: Set `metrics.enabled` and `metrics.serviceMonitor.enabled`.
+# TODO: Add schedules.
+resource "helm_release" "velero" {
+  name      = "velero"
+  chart     = "stable/velero"
+  namespace = "velero"
+
+  set {
+    name  = "configuration.backupStorageLocation.name"
+    value = "aws"
+  }
+
+  set {
+    name  = "configuration.backupStorageLocation.bucket"
+    value = aws_s3_bucket.velero.id
+  }
+
+  set {
+    name  = "configuration.backupStorageLocation.config.region"
+    value = aws_s3_bucket.velero.region
+  }
+
+  set {
+    name  = "configuration.provider"
+    value = "aws"
+  }
+
+  set_sensitive {
+    name  = "credentials.secretContents.cloud"
+    value = format("[default]\naws_access_key_id=%s\naws_secret_access_key=%s", aws_iam_access_key.velero.id, aws_iam_access_key.velero.secret)
+  }
+
+  set {
+    name  = "snapshotsEnabled"
+    value = false
+  }
+}
