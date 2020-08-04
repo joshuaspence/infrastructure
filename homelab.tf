d9048949ab26c59f1650c260714aef22f52a836e
Joshua Spence <josh@joshuaspence.com>
2020-08-04 22:55:02 +1000

    Add IAM user for `external-dns`

--- homelab.tf
+++ homelab.tf
@@ -32,3 +32,32 @@ resource "aws_iam_user_policy" "cert_manager" {
   user   = aws_iam_user.cert_manager.name
   policy = data.aws_iam_policy_document.cert_manager.json
 }
+
+resource "aws_iam_user" "external_dns" {
+  name = "external-dns"
+}
+
+resource "aws_iam_access_key" "external_dns" {
+  user = aws_iam_user.external_dns.name
+}
+
+data "aws_iam_policy_document" "external_dns" {
+  statement {
+    actions = [
+      "route53:ChangeResourceRecordSets",
+      "route53:ListResourceRecordSets",
+    ]
+
+    resources = ["arn:aws:route53:::hostedzone/*"]
+  }
+
+  statement {
+    actions = ["route53:ListHostedZones"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_user_policy" "external_dns" {
+  user   = aws_iam_user.external_dns.name
+  policy = data.aws_iam_policy_document.external_dns.json
+}
