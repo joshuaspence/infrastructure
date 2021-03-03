9455419b32b49026766868c6a2ff48b788d6318c
Joshua Spence <josh@joshuaspence.com>
2021-03-03 12:19:56 +1100

    Minor

--- unifi.tf
+++ unifi.tf
@@ -17,13 +17,12 @@ resource "unifi_network" "wan" {
   name    = "WAN"
   purpose = "wan"
 
-  wan_ip           = "192.168.1.1"
   wan_networkgroup = "WAN"
   wan_type         = "dhcp"
 
   # TODO: Remove this after https://github.com/paultyng/terraform-provider-unifi/issues/107.
   lifecycle {
-    ignore_changes = [dhcp_lease, ipv6_interface_type, network_group]
+    ignore_changes = [dhcp_lease, ipv6_interface_type, network_group, wan_ip]
   }
 }
 
