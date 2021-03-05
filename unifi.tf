9bc9ef145ecd06667879ab9415d6bd840d2ce2d1
Joshua Spence <josh@joshuaspence.com>
2021-03-05 20:07:43 +1100

    Add a TODO comment

--- unifi.tf
+++ unifi.tf
@@ -35,3 +35,14 @@ resource "unifi_user" "client" {
 
   for_each = var.unifi_clients
 }
+
+/**
+ * TODO: Manage data from the following API endpoints:
+ *
+ * - `/api/s/${SITE}/rest/dynamicdns`
+ * - `/api/s/${SITE}/rest/routing`
+ * - `/api/s/${SITE}/rest/setting`
+ * - `/api/s/${SITE}/self`
+ * - `/api/s/${SITE}/stat/device` / `/api/s/${SITE}/rest/device/${ID}`
+ * - `/api/s/${SITE}/stat/sysinfo`
+ */
