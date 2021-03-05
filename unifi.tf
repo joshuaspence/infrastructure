a5fb1749d353ff147375d1c1e83c654a38230af3
Joshua Spence <josh@joshuaspence.com>
2021-03-05 12:23:22 +1100

    Allow ICMP Ping on WAN interface

--- unifi.tf
+++ unifi.tf
@@ -6,19 +6,6 @@ resource "unifi_user_group" "default" {
   name = "Default"
 }
 
-resource "unifi_network" "wan" {
-  name    = "WAN"
-  purpose = "wan"
-
-  wan_networkgroup = "WAN"
-  wan_type         = "dhcp"
-
-  # TODO: Remove this after https://github.com/paultyng/terraform-provider-unifi/issues/107.
-  lifecycle {
-    ignore_changes = [dhcp_lease, ipv6_interface_type, network_group, wan_ip]
-  }
-}
-
 variable "unifi_clients" {
   type = map(object({
     mac  = string
---
+++ unifi_wan.tf
@@ -0,0 +1,21 @@
+resource "unifi_network" "wan" {
+  name    = "WAN"
+  purpose = "wan"
+
+  wan_networkgroup = "WAN"
+  wan_type         = "dhcp"
+
+  # TODO: Remove this after https://github.com/paultyng/terraform-provider-unifi/issues/107.
+  lifecycle {
+    ignore_changes = [dhcp_lease, ipv6_interface_type, network_group, wan_ip]
+  }
+}
+
+# TODO: Set `icmp_typename`, see https://github.com/paultyng/terraform-provider-unifi/pull/108.
+resource "unifi_firewall_rule" "wan_ping" {
+  name       = "ICMP Ping"
+  ruleset    = "WAN_LOCAL"
+  rule_index = 2000
+  action     = "accept"
+  protocol   = "icmp"
+}
