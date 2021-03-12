6305071dec1916490147cdb8b438f5d19201f498
Joshua Spence <josh@joshuaspence.com>
2021-03-12 20:02:52 +1100

    Remove `unifi_firewall_rule.wan_ping`

--- unifi_wan.tf
+++ unifi_wan.tf
@@ -10,13 +10,3 @@ resource "unifi_network" "wan" {
     ignore_changes = [dhcp_lease, ipv6_interface_type, network_group, wan_ip]
   }
 }
-
-# TODO: Set `icmp_typename`, see https://github.com/paultyng/terraform-provider-unifi/pull/108.
-resource "unifi_firewall_rule" "wan_ping" {
-  name          = "ICMP Ping"
-  ruleset       = "WAN_LOCAL"
-  rule_index    = 2000
-  action        = "accept"
-  protocol      = "icmp"
-  icmp_typename = "echo-request"
-}
