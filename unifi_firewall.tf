708a3c21ce04f49edd059f1f6889e695fb968a4b
Joshua Spence <josh@joshuaspence.com>
2021-03-12 18:57:10 +1100

    Allow NTP for NoT network

--- unifi_firewall.tf
+++ unifi_firewall.tf
@@ -28,11 +28,23 @@ resource "unifi_firewall_rule" "lan" {
   src_firewall_group_ids = [unifi_firewall_group.iot_not.id]
 }
 
+resource "unifi_firewall_rule" "not_ntp" {
+  name       = "Allow outbound NoT NTP traffic"
+  action     = "accept"
+  protocol   = "udp"
+  rule_index = 2000
+  ruleset    = "WAN_OUT"
+  logging    = true
+
+  src_network_id = unifi_network.not.id
+  dst_port       = 123
+}
+
 resource "unifi_firewall_rule" "not_wan" {
   name       = "Drop outbound NoT WAN traffic"
   action     = "drop"
   protocol   = "all"
-  rule_index = 2000
+  rule_index = 2100
   ruleset    = "WAN_OUT"
   logging    = true
 
