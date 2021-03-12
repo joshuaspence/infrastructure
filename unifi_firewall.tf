c2e7f4c1a5e66ff795979b41d80261960a8db96e
Joshua Spence <josh@joshuaspence.com>
2021-03-12 14:09:53 +1100

    Block LAN traffic from the NoT network

--- unifi_firewall.tf
+++ unifi_firewall.tf
@@ -1,5 +1,28 @@
+resource "unifi_firewall_rule" "not_hass" {
+  name       = "Allow NoT LAN to Home Assistant"
+  action     = "accept"
+  protocol   = "all"
+  rule_index = 2000
+  ruleset    = "LAN_OUT"
+
+  src_network_id    = unifi_network.not.id
+  dst_address       = unifi_user.client["home_assistant"].ip
+  state_established = true
+  state_related     = true
+}
+
+resource "unifi_firewall_rule" "not_lan" {
+  name           = "Drop outbound NoT LAN traffic"
+  action         = "drop"
+  protocol       = "all"
+  rule_index     = 2001
+  ruleset        = "LAN_OUT"
+  logging        = true
+  src_network_id = unifi_network.not.id
+}
+
 resource "unifi_firewall_rule" "not_wan" {
-  name           = "Drop outbound NoT traffic"
+  name           = "Drop outbound NoT WAN traffic"
   action         = "drop"
   protocol       = "all"
   rule_index     = 2000
