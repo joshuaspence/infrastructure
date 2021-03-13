8abde58e8a1f8435ecba8994974d1e6537457787
Joshua Spence <josh@joshuaspence.com>
2021-03-13 19:27:21 +1100

    Restructure

--- unifi/lan.tf
+++ unifi/lan.tf
@@ -11,28 +11,14 @@ resource "unifi_network" "main" {
   dhcp_stop     = cidrhost(var.networks.main.subnet, -2)
 }
 
-resource "unifi_network" "iot" {
-  name    = "IoT"
-  purpose = "corporate"
+resource "unifi_wlan" "main" {
+  name          = var.wlans.main.ssid
+  security      = "wpapsk"
+  passphrase    = var.wlans.main.passphrase
+  network_id    = unifi_network.main.id
+  ap_group_ids  = [data.unifi_ap_group.default.id]
+  user_group_id = unifi_user_group.default.id
 
-  network_group = "LAN"
-  vlan_id       = var.networks.iot.vlan
-  subnet        = var.networks.iot.subnet
-  domain_name   = "local"
-  dhcp_enabled  = true
-  dhcp_start    = cidrhost(var.networks.iot.subnet, 6)
-  dhcp_stop     = cidrhost(var.networks.iot.subnet, -2)
-}
-
-resource "unifi_network" "not" {
-  name    = "NoT"
-  purpose = "corporate"
-
-  network_group = "LAN"
-  vlan_id       = var.networks.not.vlan
-  subnet        = var.networks.not.subnet
-  domain_name   = "local"
-  dhcp_enabled  = true
-  dhcp_start    = cidrhost(var.networks.not.subnet, 6)
-  dhcp_stop     = cidrhost(var.networks.not.subnet, -2)
+  no2ghz_oui = false
+  wlan_band  = "both"
 }
---
+++ unifi/lan_not.tf
@@ -0,0 +1,24 @@
+resource "unifi_network" "not" {
+  name    = "NoT"
+  purpose = "corporate"
+
+  network_group = "LAN"
+  vlan_id       = var.networks.not.vlan
+  subnet        = var.networks.not.subnet
+  domain_name   = "local"
+  dhcp_enabled  = true
+  dhcp_start    = cidrhost(var.networks.not.subnet, 6)
+  dhcp_stop     = cidrhost(var.networks.not.subnet, -2)
+}
+
+resource "unifi_wlan" "not" {
+  name          = var.wlans.not.ssid
+  security      = "wpapsk"
+  passphrase    = var.wlans.not.passphrase
+  network_id    = unifi_network.not.id
+  ap_group_ids  = [data.unifi_ap_group.default.id]
+  user_group_id = unifi_user_group.default.id
+
+  no2ghz_oui = false
+  wlan_band  = "both"
+}
