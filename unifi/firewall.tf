89d951509a87c31a7dfb0f21255d35eb19304242
Joshua Spence <josh@joshuaspence.com>
2021-03-19 16:55:21 +1100

    Tweak parameter ordering

--- unifi/firewall.tf
+++ unifi/firewall.tf
@@ -19,22 +19,22 @@ resource "unifi_firewall_rule" "lan_established" {
 }
 
 resource "unifi_firewall_rule" "lan_outbound" {
+  ruleset    = "LAN_IN"
+  rule_index = 2100
   name       = "Drop outbound IoT/NoT LAN traffic"
   action     = "drop"
   protocol   = "all"
-  rule_index = 2100
-  ruleset    = "LAN_IN"
   logging    = true
 
   src_firewall_group_ids = [unifi_firewall_group.iot_not.id]
 }
 
 resource "unifi_firewall_rule" "not_ntp" {
+  ruleset    = "WAN_OUT"
+  rule_index = 2000
   name       = "Allow outbound NoT NTP traffic"
   action     = "accept"
   protocol   = "udp"
-  rule_index = 2000
-  ruleset    = "WAN_OUT"
 
   src_network_id = unifi_network.network["not"].id
   dst_port       = 123
@@ -47,22 +47,22 @@ resource "unifi_firewall_group" "not_wan_silent" {
 }
 
 resource "unifi_firewall_rule" "not_wan_silent" {
+  ruleset    = "WAN_OUT"
+  rule_index = 2100
   name       = "Drop outbound NoT WAN traffic"
   action     = "drop"
   protocol   = "all"
-  rule_index = 2100
-  ruleset    = "WAN_OUT"
 
   src_network_id         = unifi_network.network["not"].id
   dst_firewall_group_ids = [unifi_firewall_group.not_wan_silent.id]
 }
 
 resource "unifi_firewall_rule" "not_wan_logged" {
+  ruleset    = "WAN_OUT"
+  rule_index = 2101
   name       = "Drop outbound NoT WAN traffic"
   action     = "drop"
   protocol   = "all"
-  rule_index = 2101
-  ruleset    = "WAN_OUT"
   logging    = true
 
   src_network_id = unifi_network.network["not"].id
