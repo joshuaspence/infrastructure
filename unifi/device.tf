a630aed0056ca609e8b8875efade0addc27227d8
Joshua Spence <josh@joshuaspence.com>
2021-04-27 16:15:06 +1000

    Add new access point

--- unifi/device.tf
+++ unifi/device.tf
@@ -2,6 +2,10 @@ resource "unifi_device" "hallway_access_point" {
   name = "Hallway Access Point"
 }
 
+resource "unifi_device" "kitchen_access_point" {
+  name = "Kitchen Access Point"
+}
+
 resource "unifi_device" "gateway" {
   name = "Gateway"
 }
