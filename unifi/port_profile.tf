797f4c4bfda02d8d8534445e304d81097fa959e7
Joshua Spence <josh@joshuaspence.com>
2021-06-10 14:20:03 +1000

    Add default port profiles

--- unifi/port_profile.tf
+++ unifi/port_profile.tf
@@ -1,7 +1,17 @@
 locals {
   port_profiles = {
+    all          = data.unifi_port_profile.all
+    disabled     = data.unifi_port_profile.disabled
     poe_disabled = unifi_port_profile.poe_disabled
   }
+}
+
+data "unifi_port_profile" "all" {
+  name = "All"
+}
+
+data "unifi_port_profile" "disabled" {
+  name = "Disabled"
 }
 
 resource "unifi_port_profile" "poe_disabled" {
