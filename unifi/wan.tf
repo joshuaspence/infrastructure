ce20f9b57ca058a67f709d3d6f36d1cb755b6c3b
Joshua Spence <josh@joshuaspence.com>
2021-10-13 09:25:10 +1100

    Add failover WAN network

--- unifi/wan.tf
+++ unifi/wan.tf
@@ -11,3 +11,17 @@ resource "unifi_network" "wan" {
     ignore_changes = [dhcp_lease, ipv6_interface_type, network_group]
   }
 }
+
+resource "unifi_network" "failover_wan" {
+  name    = "Failover WAN"
+  purpose = "wan"
+
+  wan_networkgroup = "WAN2"
+  wan_type         = "dhcp"
+  wan_dns          = ["1.1.1.1", "8.8.8.8"]
+
+  # TODO: Remove this after https://github.com/paultyng/terraform-provider-unifi/issues/107.
+  lifecycle {
+    ignore_changes = [dhcp_lease, ipv6_interface_type, network_group]
+  }
+}
