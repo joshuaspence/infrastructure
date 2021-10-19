d85242f7f6d129f7350cd49ba0fb49e299f5ac7f
Joshua Spence <josh@joshuaspence.com>
2021-10-19 12:21:13 +1100

    Change failover WAN subnet

--- unifi/wan.tf
+++ unifi/wan.tf
@@ -17,26 +17,13 @@ resource "unifi_network" "failover_wan" {
   purpose = "wan"
 
   wan_networkgroup = "WAN2"
-  wan_type         = "dhcp"
+  wan_type         = "static"
+  wan_ip           = "192.168.200.1"
+  wan_netmask      = "255.255.255.0"
   wan_dns          = ["1.1.1.1", "8.8.8.8"]
 
   # TODO: Remove this after https://github.com/paultyng/terraform-provider-unifi/issues/107.
   lifecycle {
     ignore_changes = [dhcp_lease, ipv6_interface_type, network_group]
-  }
-}
-
-# TODO: Improve this.
-resource "unifi_static_route" "failover_wan" {
-  name      = "Failover WAN Admin UI"
-  network   = "192.168.200.0/24"
-  type      = "interface-route"
-  interface = unifi_network.failover_wan.wan_networkgroup
-
-  # TODO: This shouldn't be needed.
-  distance = 1
-
-  lifecycle {
-    ignore_changes = [distance]
   }
 }
