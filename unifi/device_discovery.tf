e99e571ecd0fedcaf10d78cf1bcfe817e59a4f95
Joshua Spence <josh@joshuaspence.com>
2022-02-10 15:01:49 +1100

    Add firewall rules for Daikin Airbase

--- unifi/device_discovery.tf
+++ unifi/device_discovery.tf
@@ -3,13 +3,20 @@ locals {
 
   device_discovery = [
     {
-      name = "TP-Link Kasa"
-      port = 9999
+      name           = "TP-Link Kasa"
+      broadcast_port = 9999
+      source_port    = 9999
     },
     {
-      name = "Tuya"
-      port = 6667
+      name             = "Tuya"
+      broadcast_port   = 6667
+      destination_port = 6667
     },
+    {
+      name             = "Daikin Airbase"
+      broadcast_port   = 30050
+      destination_port = 30000
+    }
   ]
 
   bcast_relay_config = {
@@ -17,26 +24,44 @@ locals {
       for index, device_discovery in local.device_discovery : index + 1 => {
         description = "${device_discovery.name} Discovery"
         interface   = local.gateway_lan_interfaces
-        port        = device_discovery.port
+        port        = device_discovery.broadcast_port
       }
     }
   }
 }
 
-resource "unifi_firewall_group" "device_discovery" {
-  name    = "Device Discovery"
+resource "unifi_firewall_group" "device_discovery_source" {
+  name    = "Device Discovery (Source)"
   type    = "port-group"
-  members = [for device_discovery in local.device_discovery : device_discovery.port]
+  members = [for device_discovery in local.device_discovery : device_discovery.source_port if can(device_discovery.source_port)]
 }
 
-resource "unifi_firewall_rule" "device_discovery" {
+resource "unifi_firewall_group" "device_discovery_destination" {
+  name    = "Device Discovery (Destination)"
+  type    = "port-group"
+  members = [for device_discovery in local.device_discovery : device_discovery.destination_port if can(device_discovery.destination_port)]
+}
+
+resource "unifi_firewall_rule" "device_discovery_source" {
   ruleset    = "LAN_OUT"
   rule_index = 2003
 
-  name     = "Allow device discovery"
+  name     = "Allow device discovery (source)"
   action   = "accept"
   protocol = "udp"
 
-  src_firewall_group_ids = [unifi_firewall_group.device_discovery.id]
+  src_firewall_group_ids = [unifi_firewall_group.device_discovery_source.id]
+  dst_network_id         = unifi_network.network["main"].id
+}
+
+resource "unifi_firewall_rule" "device_discovery_destination" {
+  ruleset    = "LAN_OUT"
+  rule_index = 2004
+
+  name     = "Allow device discovery (destination)"
+  action   = "accept"
+  protocol = "udp"
+
+  dst_firewall_group_ids = [unifi_firewall_group.device_discovery_destination.id]
   dst_network_id         = unifi_network.network["main"].id
 }
