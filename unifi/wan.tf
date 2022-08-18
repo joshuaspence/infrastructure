40db496db7451c70b63c11a870c84a2a1972e0ed
Joshua Spence <josh@spence.com.au>
2022-08-18 13:47:57 +1000

    Use DHCP for failover WAN

--- unifi/wan.tf
+++ unifi/wan.tf
@@ -1,6 +1,5 @@
 locals {
-  failover_wan_subnet = "192.168.200.0/24"
-  wan_dns             = ["1.1.1.1", "8.8.8.8"]
+  wan_dns = ["1.1.1.1", "8.8.8.8"]
 }
 
 # TODO: Add IPv6 settings.
@@ -18,28 +17,17 @@ resource "unifi_network" "wan" {
   }
 }
 
+# TODO: Create traffic route for `192.168.0.2`.
 resource "unifi_network" "failover_wan" {
   name    = "Failover WAN"
   purpose = "wan"
 
   wan_networkgroup = "WAN2"
-  wan_type         = "static"
-  wan_ip           = cidrhost(local.failover_wan_subnet, 2)
-  wan_netmask      = cidrnetmask(local.failover_wan_subnet)
-  wan_gateway      = cidrhost(local.failover_wan_subnet, 1)
+  wan_type         = "dhcp"
   wan_dns          = local.wan_dns
 
   # TODO: Remove this after https://github.com/paultyng/terraform-provider-unifi/issues/107.
   lifecycle {
     ignore_changes = [dhcp_lease, ipv6_interface_type, network_group]
   }
-}
-
-# TODO: Is this actually needed?
-resource "unifi_static_route" "failover_wan" {
-  name     = "Failover WAN Admin UI"
-  type     = "nexthop-route"
-  network  = local.failover_wan_subnet
-  distance = 1
-  next_hop = unifi_network.failover_wan.wan_ip
 }
