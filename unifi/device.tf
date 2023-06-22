8bca6b7b3cbfe2c3545e8aca516df28d31821501
Joshua Spence <josh@spence.com.au>
2023-06-22 12:39:00 +1000

    Change switch port names

--- unifi/device.tf
+++ unifi/device.tf
@@ -1,85 +1,40 @@
-locals {
-  access_point_ports = {
-    for key, device in var.access_points : key => merge(
-      {
-        for idx in range(1, device.ports + 1) : idx => {
-          name    = null
-          profile = "disabled"
-        }
-      },
-      {
-        for client_key, client in var.clients : client.uplink.port => {
-          name    = unifi_user.client[client_key].name
-          profile = client.uplink.profile
-        } if try(client.uplink.access_point, null) == key
-      },
-    )
-  }
-}
-
 resource "unifi_device" "access_point" {
   name = format("%s Access Point", title(replace(each.key, "_", " ")))
   mac  = each.value.mac
 
   dynamic "port_override" {
-    for_each = local.access_point_ports[each.key]
-    iterator = port
+    for_each = {
+      for idx in range(1, each.value.ports + 1) : idx => {
+        name = null
+      }
+    }
 
     content {
-      name   = port.value.name
-      number = port.key
+      name   = port_override.value.name
+      number = port_override.key
     }
   }
 
   for_each = var.access_points
 }
 
-locals {
-  switch_ports = {
-    for key, device in var.switches : key => merge(
-      {
-        for idx in range(1, device.ports + 1) : idx => {
-          name                = null
-          profile             = "disabled"
-          op_mode             = null
-          aggregate_num_ports = null
-        }
-      },
-      {
-        for idx, port in device.port_overrides : idx => port
-      },
-      {
-        for client_key, client in var.clients : client.uplink.port => {
-          name                = unifi_user.client[client_key].name
-          profile             = client.uplink.profile
-          op_mode             = null
-          aggregate_num_ports = null
-        } if try(client.uplink.switch, null) == key
-      },
-      {
-        for device_key, device in var.access_points : device.uplink.port => {
-          name                = unifi_device.access_point[device_key].name
-          profile             = "all"
-          op_mode             = null
-          aggregate_num_ports = null
-        } if device.uplink.switch == key
-      },
-    )
-  }
-}
-
 resource "unifi_device" "switch" {
   name = each.value.name == null ? format("%s Switch", title(replace(each.key, "_", " "))) : each.value.name
 
   dynamic "port_override" {
-    for_each = local.switch_ports[each.key]
-    iterator = port
+    for_each = {
+      for idx in range(1, each.value.ports + 1) : idx => {
+        name                = try(each.value.port_overrides[idx].name, null)
+        op_mode             = try(each.value.port_overrides[idx].op_mode, null)
+        aggregate_num_ports = try(each.value.port_overrides[idx].aggregate_num_ports, null)
+      }
+    }
 
     content {
-      name                = port.value.name
-      number              = port.key
-      op_mode             = port.value.op_mode
-      aggregate_num_ports = port.value.aggregate_num_ports
+      name                = port_override.value.name
+      number              = port_override.key
+      op_mode             = port_override.value.op_mode
+      aggregate_num_ports = port_override.value.aggregate_num_ports
     }
   }
 
