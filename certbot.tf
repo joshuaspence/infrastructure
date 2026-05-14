0105e5e06e1c7c3921304ea4a5f1bc5e47f9e808
Josh Spence <josh@spence.com.au>
2026-05-15 00:41:41 +1000

    Fix `certbot` permissions

--- certbot.tf
+++ certbot.tf
@@ -7,6 +7,7 @@ data "aws_iam_policy_document" "certbot" {
     actions = [
       "route53:GetChange",
       "route53:ListHostedZones",
+      "route53:ListHostedZonesByName",
     ]
     resources = ["*"]
   }
