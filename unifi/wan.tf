d1674414421c75e2d88e40d6e11b68146110dfeb
Josh Spence <josh@spence.com.au>
2026-09-02 09:26:10 +1000

    Tweak `unifi_wan` resources

--- unifi/wan.tf
+++ unifi/wan.tf
@@ -1,14 +1,18 @@
 resource "unifi_wan" "primary" {
-  name         = "NBN"
-  networkgroup = "WAN"
-  type         = "dhcp"
-  type_v6      = "dhcpv6"
-
-  /*
-  dhcpv6 = {
-    pd_size = 48
+  name               = "NBN"
+  networkgroup       = "WAN"
+  type               = "dhcp"
+  type_v6            = "dhcpv6"
+  setting_preference = "manual"
+  
+  dns = {
+    preference      = "manual"
+    primary         = "1.1.1.1"
+    secondary       = "8.8.8.8"
+    ipv6_preference = "manual"
+    ipv6_primary    = "2606:4700:4700::1111"
+    ipv6_secondary  = "2001:4860:4860::8888"
   }
-  */
 
   load_balance = {
     type   = "weighted"
@@ -19,18 +23,16 @@ resource "unifi_wan" "primary" {
     download_kilobits_per_second = 500 * 1000
     upload_kilobits_per_second   = 50 * 1000
   }
-
 }
 
-# TODO: Create traffic route for `192.168.0.2`.
 resource "unifi_wan" "secondary" {
-  name         = "LTE"
-  networkgroup = "WAN2"
-  type         = "dhcp"
-  type_v6      = "disabled"
+  name               = "Secondary"
+  networkgroup       = "WAN2"
+  setting_preference = "auto"
+}
 
-  load_balance = {
-    type              = "failover-only"
-    failover_priority = 2
-  }
+resource "unifi_wan" "tertiary" {
+  name               = "UniFi U5G Max"
+  networkgroup       = "WAN3"
+  setting_preference = "auto"
 }
